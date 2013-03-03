465003a1b2a89a42e8c5e502d5676375d816d937
Vedant Kumar <vk@vedantk.com>
2013-03-02 16:14:25 -0800

    get ready for eigne

--- calc.cc
+++ calc.cc
@@ -11,7 +11,7 @@ static void onMouse(int evt, int x, int y, int, void*) {
         return;
     }
     pts.push_back(Point(x, y));
-    cout << "Point #" << pts.size() << ": " << x << " " << y << "\n";
+    cout << "Point #" << pts.size() << ": " << x << " " << y << ".\n";
     return;
 }
 
@@ -19,10 +19,22 @@ int main() {
     Mat img = imread("homography.jpg", CV_LOAD_IMAGE_COLOR);
     if (!img.data) {
         cerr << "Could not open image.\n";
+        return 1;
     }
-
     imshow("Homography", img);
+
+    cout << "Select the bottom-left, bottom-right, top-left, and top-right "
+            "corners in that order. You get to pick what those orientations "
+            "mean.\n";
     setMouseCallback("Homography", onMouse, 0);
+
+    /* Compute the homography matrix. */
+    MatrixXd A(8, 8);
+    A << MatrixXd::Zero(8, 8);
+
+
+
+
     waitKey(0);
     return 0;
 }
